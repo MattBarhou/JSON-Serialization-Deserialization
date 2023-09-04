c7887db6b932f6d6653805d6a543cf04260a60be
Matthew Barhou <barhoumatthew@gmail.com>
2023-09-04 13:00:04 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ImportExportApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string filename = "student.json";
+            List<Student> students = new List<Student>();
+
+            students.Add(new Student { StudentID = "301193037", FullName = "Matthew Barhou", Email = "mbarhou@my.centennialcollege.ca" });
+
+            //Write the student to the file
+            Student.WriteFile(students, filename);
+
+            //Load the data from file
+            List<Student> newStudent = new List<Student>();
+            newStudent = Student.ReadFile(filename);
+
+            foreach (Student student in newStudent)
+            {
+                Console.WriteLine(student);
+            }
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ImportExportApp
+{
+    internal class Student
+    {
+        public string StudentID { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+
+        public override string ToString()
+        {
+            return $"StudentID: {StudentID}, Full Name: {FullName}, Email: {Email}";
+        }
+
+        public static void WriteFile(List<Student> students, string filename)
+        {
+            StreamWriter writer = new StreamWriter(filename);
+
+            foreach (Student student in students)
+            {
+                string convertedString = JsonSerializer.Serialize(student);
+                writer.Write(convertedString);
+            }         
+            writer.Close();
+            
+        }
+
+        public static List<Student> ReadFile(string filename)
+        {
+            List<Student> mystudents = new List<Student>();
+
+            StreamReader reader = new StreamReader(filename);
+
+            string deserializedString = reader.ReadLine();
+
+            if (deserializedString != null)
+            {
+                Student student = JsonSerializer.Deserialize<Student>(deserializedString);
+                mystudents.Add(student);
+                deserializedString = reader.ReadLine();
+                Console.WriteLine(student.ToString());
+            }
+            reader.Close();
+            return mystudents;
+        }
+
+
+    }
+}
